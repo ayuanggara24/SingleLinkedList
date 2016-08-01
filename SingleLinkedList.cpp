1816b17eaaeb272392b26f5c51ba6e9aac0a1d92
ayuanggara24 <ayuanggaraspentwo@gmail.com>
2016-08-01 12:10:22 +0700

    Tambah Struct

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -5,3 +5,8 @@
 using namespace std;
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+struct node {
+	int data;
+	node* next; // untuk menghubungkan dengan node lain,tipe data dibuat seperti aturan penggunaan pointer
+};
