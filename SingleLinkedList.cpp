3ceebf62ebe2cae735451dcb03fda17446a71f79
ayuanggara24 <ayuanggaraspentwo@gmail.com>
2016-08-01 12:09:29 +0700

    Edit1

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -2,3 +2,6 @@
 #include <conio.h>
 #include <stdlib.h>
 #include <iomanip> //setw()
+using namespace std;
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
